ff04dd7d2510183fc7b1eefaf14e16b4608e12b7
sebastian Montes <u201712366@upc.edu.pe>
2023-09-20 20:21:17 -0500

    feat: Implementation ViewHome

--- taller1/src/app/app-routing.module.ts
+++ taller1/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeViewComponent } from './components/home-view/home-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeViewComponent},
+  {path:'', pathMatch: 'full', redirectTo:'home'},
+  {path:'**', pathMatch:'full', redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- taller1/src/app/app.module.ts
+++ taller1/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { MaterialModule } from 'src/shared/material.module';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeViewComponent } from './components/home-view/home-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeViewComponent
   ],
   imports: [
     BrowserModule,
